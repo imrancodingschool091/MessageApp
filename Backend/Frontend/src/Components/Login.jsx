03d49fed6e7d0bfd265b5fe4743d51312bcc48ba
suman <imrancodingschool@gmail.com>
2024-06-26 22:43:03 +0530

    added

--- Backend/Frontend/src/Components/Login.jsx
+++ Backend/Frontend/src/Components/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
     };
 
     try {
-      const response = await axios.post("https://messageapp-k7nn.onrender.com", UserInfo);
+      const response = await axios.post("http://localhost:4001/user/login", UserInfo);
 
       if (response) {
         toast.success(response.data.message,{
